59769ed5708d752d175cc5f0ce2637da878b38e9
berylcl <gakiiberyl@gmail.com>
2023-06-12 12:19:11 +0100

    Adding some dynamic styles

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -16,16 +16,19 @@ const ProductList = () => {
     return (
         <section>
             <h1>Product list</h1>
-            <div key={products.id}>
+            <div className='card' key={products.id}>
                 {products.map((product) => {
                     // eslint-disable-next-line react/jsx-key
                     return (
-                        <p key={product.id}>
-                            {product.name}
+                        <p className='card' key={product.id}>
+                            <p className='id'>{product.id}</p>
+                            <p className='name'>{product.name}</p>
                             <br />
-                            <span> ${product.price}</span>
-                            <br />
-                            <span>{product.in_stock? " In Stock": " Out of stock"}</span>
+                            <div className="info">
+                                <span> ${product.price}</span>
+                                <br />
+                                <span className={product.in_stock? "instock":"unavailable"}>{product.in_stock? " In Stock": " Out of stock"}</span>
+                            </div>
                         </p>)
                 })}
             </div>
