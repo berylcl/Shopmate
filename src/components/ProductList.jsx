bfe7035c10862ae299efef8978a80371902771c7
berylcl <gakiiberyl@gmail.com>
2023-06-12 12:39:19 +0100

    Implementing in stock and out of stock buttons

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,20 +2,22 @@ import {useEffect, useState} from "react";
 import '../App.css'
 const ProductList = () => {
     const [products, setProducts] = useState([])
+    const [url, setUrl] = useState('http://localhost:3000/products')
     useEffect( () => {
-        fetch('http://localhost:3000/products')
+        fetch(url)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data)
                 setProducts(data)
             })
             .catch((error) => {
                 console.error(error)
             })
-    },[])
+    },[url])
     return (
         <section>
             <h1>Product list</h1>
+            <button onClick={ () => setUrl('http://localhost:3000/products')}>All products</button>
+            <button onClick={ () => setUrl("http://localhost:3000/products?in_stock=true")}>In stock only</button>
             <div className='card' key={products.id}>
                 {products.map((product) => {
                     // eslint-disable-next-line react/jsx-key
