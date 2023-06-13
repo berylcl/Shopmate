bb4c9853a8d32646a61a1fa3ff61a87d203b4dcc
berylcl <gakiiberyl@gmail.com>
2023-06-13 17:14:16 +0100

    Add try catch block

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,38 +1,55 @@
 import { useEffect, useState } from "react";
 import { useFetch } from "../hooks/useFetch.js";
-import '../App.css';
+import "../App.css";
+import "./loadingSpinner.css";
 
 const ProductList = () => {
-    const [url, setUrl] = useState('http://localhost:3000/products');
+    const [url, setUrl] = useState("http://localhost:3000/products");
     const { data: products, loading } = useFetch(url);
 
     return (
         <section>
             <h1>Product list</h1>
             <button>{products ? products.length : 0}</button>
-            <button onClick={() => setUrl('http://localhost:3000/products')}>
+            <button onClick={() => setUrl("http://localhost:3000/products")}>
                 All products
             </button>
-            <button onClick={() => setUrl("http://localhost:3000/products?in_stock=true")}>
+            <button
+                onClick={() =>
+                    setUrl("http://localhost:3000/products?in_stock=true")
+                }
+            >
                 In stock only
             </button>
-            {loading && <p>I am loading</p>}
-            {products && (
-                <div className='card'>
-                    {products.map((product) => (
-                        <p className='card' key={product.id}>
-                            <p className='id'>{product.id}</p>
-                            <p className='name'>{product.name}</p>
-                            <br />
-                            <div className="info">
-                                <span> ${product.price}</span>
-                                <br />
-                                <span className={product.in_stock ? "instock" : "unavailable"}>
-                  {product.in_stock ? " In Stock" : " Out of stock"}
-                </span>
-                            </div>
-                        </p>
-                    ))}
+
+            {loading ? (
+                <div className="loadingSpinner">
+                    <div className="spinner"></div>
+                </div>
+            ) : (
+                <div>
+                    {products && (
+                        <div className="card">
+                            {products.map((product) => (
+                                <p className="card" key={product.id}>
+                                    <p className="id">{product.id}</p>
+                                    <p className="name">{product.name}</p>
+                                    <br />
+                                    <div className="info">
+                                        <span>${product.price}</span>
+                                        <br />
+                                        <span
+                                            className={
+                                                product.in_stock ? "instock" : "unavailable"
+                                            }
+                                        >
+                      {product.in_stock ? " In Stock" : " Out of stock"}
+                    </span>
+                                    </div>
+                                </p>
+                            ))}
+                        </div>
+                    )}
                 </div>
             )}
         </section>
@@ -40,3 +57,4 @@ const ProductList = () => {
 };
 
 export default ProductList;
+
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,15 +3,24 @@ import {useEffect, useState} from "react";
 export const useFetch = (url) => {
     const [data, setData] = useState(null)
     const [loading,setLoading] = useState(false)
+    const [error, setError] = useState(null)
     useEffect(()=>{
         const fetchData = async () => {
-            setLoading(true)
-            const response = await fetch(url)
-            const result = await response.json()
-            setLoading(false)
-            setData(result)
+            try {
+                setLoading(true)
+                const response = await fetch(url)
+                if(!response.ok) {
+                    throw new Error("Network response was not okay")
+                }
+                const result = await response.json()
+                setLoading(false)
+                setData(result)
+            } catch (error) {
+                setError(error)
+                setLoading(false)
+            }
         }
         fetchData()
     },[url])
-    return {data, loading}
+    return {data, loading, error}
 }
